e7e094b887a0af59d73eeabbc0aa1e29feb9e493
alekswatts <aleksmytiuk@gmail.com>
2019-11-20 00:23:15 +0200

    last file

--- p4u/4_dz/google.py
+++ p4u/4_dz/google.py
@@ -3,22 +3,20 @@ from time import time
 
 domain = 'https://netflix.com'
 t1 = time()
-count = 0
 
 with open('netflix_keys.txt', 'r', encoding='utf-8') as f:
     with open('results.csv', 'w', encoding='utf-8') as f2:
         f2.write(f'Keyword\tDomain\tPosition\n')
         for line in f:
             line = line.strip()
-            session = HTMLSession()
-            resp = session.get(f'https://www.google.com/search?q={line}&num=100&hl=en')
+            with HTMLSession() as session:
+                resp = session.get(f'https://www.google.com/search?q={line}&num=100&hl=en')
             links = resp.html.xpath('//div[@class="r"]/a[1]/@href')
 
             for position, url in enumerate(links, start=1):
                 if domain in url:
                     result = f'{line.strip()}\t{url}\t{position}\n'
                     f2.write(result)
-
 t2 = time()
 print(f'All done!')
-print('Working time is: ', round(t2-t1, 4), ' seconds')
+print('Working time is: ', round(t2 - t1, 4), ' seconds')
