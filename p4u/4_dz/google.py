f4ee03417068d6be79e409bea881c1a7d1a4cded
alekswatts <aleksmytiuk@gmail.com>
2019-11-19 13:16:50 +0200

    parser

--- p4u/4_dz/google.py
+++ p4u/4_dz/google.py
@@ -1,17 +1,24 @@
-from pprint import pprint
 from requests_html import HTMLSession
+from time import time
 
+domain = 'https://netflix.com'
+t1 = time()
+count = 0
 
-keyword = input('Введите ключевое слово: ')
-domain = input('Введите домен: ')
+with open('netflix_keys.txt', 'r', encoding='utf-8') as f:
+    with open('results.csv', 'w', encoding='utf-8') as f2:
+        f2.write(f'Keyword\tDomain\tPosition\n')
+        for line in f:
+            line = line.strip()
+            session = HTMLSession()
+            resp = session.get(f'https://www.google.com/search?q={line}&num=100&hl=en')
+            links = resp.html.xpath('//div[@class="r"]/a[1]/@href')
 
-session = HTMLSession()
+            for position, url in enumerate(links, start=1):
+                if domain in url:
+                    result = f'{line.strip()}\t{url}\t{position}\n'
+                    f2.write(result)
 
-resp = session.get(
-	f'https://www.google.com/search?q={keyword}&num=100&hl=en')
-
-links = resp.html.xpath('//div[@class="r"]/a[1]/@href')
-
-for position, url in enumerate(links, start=1):
-	if domain in url:
-		print(f'Position for website {domain} is {position}')
+t2 = time()
+print(f'All done!')
+print('Working time is: ', round(t2-t1, 4), ' seconds')
