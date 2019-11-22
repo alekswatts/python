132e9a31f1064096885e40ad8bd8ef79ad440851
philosopher <AleksMytiuk@gmail.com>
2019-11-22 13:50:50 +0200

    function

--- p4u/4_dz/bukvariks.py
+++ p4u/4_dz/bukvariks.py
@@ -2,36 +2,38 @@ from time import time
 
 keys = input('Enter keyword or comma separated keywords: ')
 t1 = time()
-count = 0
-# з залальної бази букварікса викачав слова "keywords" - скоріше обробляє
-with open('keyword.txt', 'r', encoding='utf-8') as f:
-    with open('results.csv', 'w', encoding='utf-8') as f2:
 
-        f2.write(f'Keyword\tWords Count\tSymbols\n')
-        for line in f:
-            line = line.strip()
-            temp_list = line.split()
 
-            flags = list()
-            for kw in keys.split(', '):
-                if kw in temp_list:
-                    flags.append(True)
-                else:
-                    flags.append(False)
+def keyword_file():
+    count = 0
+    with open('keyword.txt', 'r', encoding='utf-8') as f:
+        with open('results.csv', 'w', encoding='utf-8') as f2:
 
-            if any(flags):
+            f2.write(f'Keyword\tWords Count\tSymbols\n')
+            for line in f:
+                line = line.strip()
+                temp_list = line.split()
 
-                words_count = len(line.split())
-                symbols_count = len(line)
-                raw = f'{line}\t{words_count}\t{symbols_count}\n'
-                count +=1
-                f2.write(raw)
+                flags = list()
+                for kw in keys.split(', '):
+                    if kw in temp_list:
+                        flags.append(True)
+                    else:
+                        flags.append(False)
 
-                del words_count, symbols_count, raw
+                if any(flags):
+                    words_count = len(line.split())
+                    symbols_count = len(line)
+                    raw = f'{line}\t{words_count}\t{symbols_count}\n'
+                    count += 1
+                    f2.write(raw)
 
-                del temp_list
+                    del words_count, symbols_count, raw
+                    del temp_list
+    return print(f'All done! Found {count} keywords')
+
+
+keyword_file()
+
 t2 = time()
-
-print(f'All done! Found {count} keywords')
-print('Working time is: ', round(t2-t1, 4), ' seconds')
-
+print('Working time is: ', round(t2 - t1, 4), ' seconds')
