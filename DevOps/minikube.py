66588bbb8a4e243b568e8bd6815e0041832aa3d9
alan <alan@myhost>
2020-03-21 16:34:01 +0200

    finished minikube api version control

--- DevOps/minikube.py
+++ DevOps/minikube.py
@@ -1,16 +1,29 @@
-from kubernetes import client, config, watch
+from requests_html import HTMLSession
+import json
 
-# Configs can be set in Configuration class directly or using helper utility
-config.load_kube_config()
 
-v1 = client.CoreV1Api()
-count = 10
-w = watch.Watch()
-for event in w.stream(v1.list_namespace, _request_timeout=60):
-    print("Event: %s %s" % (event['type'], event['object'].metadata.name))
-    count -= 1
-    if not count:
-        w.stop()
+def main_data():
+    from kubernetes import client, config
 
-print("Ended.")
-print(v1)
+    config.load_kube_config()
+
+    v1 = client.CoreV1Api()
+    print("Listing pods with their IPs:")
+    ret = v1.list_pod_for_all_namespaces(watch=False)
+    for i in ret.items:
+        print("%s\t%s\t%s" % (i.status.pod_ip, i.metadata.namespace, i.metadata.name))
+
+
+with HTMLSession() as session:
+    response = session.get("http://localhost:8080/api/")
+
+
+def jprint(response):
+    api = json.dumps(response, sort_keys=True, indent=4)
+    unpack = json.loads(api)
+    for key in unpack:
+        if key == 'versions':
+            print('Minikube version is: ', unpack[key])
+
+
+jprint(response.json())
