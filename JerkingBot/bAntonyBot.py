00efe0a34f527707d1549a3953da3502c80d621b
alan <alan@myhost>
2020-04-20 22:27:51 +0300

    new way for parsing

---
+++ JerkingBot/bAntonyBot.py
@@ -0,0 +1,13 @@
+# def download_image(url):
+#     name = random.randrange(1, 100)
+#     fullname = str(name) + ".jpg"
+#     urllib.request.urlretrieve(url, fullname)
+#
+#
+# download_image("https://img5.xuk.life/images/photos/00/05/13/94/51394/origin/fbe6b13767ef33c9fac405e0dbcbfa89.jpg")
+
+page = 'https://xuk.life/'
+
+html_page = urllib.request.urlopen(page)
+soup = BeautifulSoup(html_page, "lxml")
+print(soup)
