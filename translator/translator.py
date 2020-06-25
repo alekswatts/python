ff87c476e159e2645dc2ac3a2810edd2b7b6adcc
philosopher <AleksMytiuk@gmail.com>
2020-06-25 13:08:46 +0300

    update

--- translator/translator.py
+++
@@ -1,6 +0,0 @@
-from google.cloud import translate
-
-filename = input('Enter filename: ')
-
-with open('filename.doc', 'w', encoding='utf-8') as f:
-    with open('filename.csv', 'w', encoding='utf-8') as f2:
