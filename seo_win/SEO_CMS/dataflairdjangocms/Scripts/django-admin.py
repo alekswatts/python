2dd124f410a6da6d4dd90de2a3c73f15d1b08862
philosopher <AleksMytiuk@gmail.com>
2021-07-15 13:04:11 +0300

    fuck

--- seo_win/SEO_CMS/dataflairdjangocms/Scripts/django-admin.py
+++
@@ -1,5 +0,0 @@
-#!C:\Users\User\Google Диск\SEO Work\Repositories\python\seo_win\SEO_CMS\dataflairdjangocms\Scripts\python.exe
-from django.core import management
-
-if __name__ == "__main__":
-    management.execute_from_command_line()
